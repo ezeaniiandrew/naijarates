74cd95736f106a32b4e4787f9832fbffa0f9e15f
ezeaniiandrew <ezeaniiandrew@gmail.com>
2026-03-18 12:47:47 +0100

    chore: migrate hosting from Cloudflare workers to Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
   root: ".",
-  plugins: [cloudflare()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
